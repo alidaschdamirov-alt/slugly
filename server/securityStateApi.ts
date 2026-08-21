cf42b002480b77a87da9e9a0167dc045bcfeb0e1
alidaschdamirov-alt <alidaschdamirov@gmail.com>
2026-08-21 10:36:31 +0400

    feat(audit): mount advanced audit explorer API

--- server/securityStateApi.ts
+++ server/securityStateApi.ts
@@ -1,3 +1,4 @@
+import { auditExplorerRouter } from "./auditExplorerApi";
 import { backupOperationsRouter } from "./backupOperationsApi";
 import { dangerousActionsRouter } from "./dangerousActionsApi";
 import { emailDeliverabilityRouter } from "./emailDeliverabilityApi";
@@ -6,5 +7,6 @@ import { securityStateRouter as coreSecurityStateRouter } from "./securityStateC
 coreSecurityStateRouter.use("/dangerous-actions", dangerousActionsRouter);
 coreSecurityStateRouter.use("/email", emailDeliverabilityRouter);
 coreSecurityStateRouter.use("/backups", backupOperationsRouter);
+coreSecurityStateRouter.use("/audit", auditExplorerRouter);
 
 export const securityStateRouter = coreSecurityStateRouter;
