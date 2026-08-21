bc80843b54a23767f39a37ddb726b6873f631f36
alidaschdamirov-alt <alidaschdamirov@gmail.com>
2026-08-21 10:28:37 +0400

    feat(backups): mount privileged backup operations

--- server/securityStateApi.ts
+++ server/securityStateApi.ts
@@ -1,8 +1,10 @@
+import { backupOperationsRouter } from "./backupOperationsApi";
 import { dangerousActionsRouter } from "./dangerousActionsApi";
 import { emailDeliverabilityRouter } from "./emailDeliverabilityApi";
 import { securityStateRouter as coreSecurityStateRouter } from "./securityStateCore";
 
 coreSecurityStateRouter.use("/dangerous-actions", dangerousActionsRouter);
 coreSecurityStateRouter.use("/email", emailDeliverabilityRouter);
+coreSecurityStateRouter.use("/backups", backupOperationsRouter);
 
 export const securityStateRouter = coreSecurityStateRouter;
