81422619825a591bb4548920aa275ba0398d5934
alidaschdamirov-alt <alidaschdamirov@gmail.com>
2026-08-21 10:19:08 +0400

    feat(email): mount deliverability webhook and metrics routes

--- server/securityStateApi.ts
+++ server/securityStateApi.ts
@@ -1,6 +1,8 @@
 import { dangerousActionsRouter } from "./dangerousActionsApi";
+import { emailDeliverabilityRouter } from "./emailDeliverabilityApi";
 import { securityStateRouter as coreSecurityStateRouter } from "./securityStateCore";
 
 coreSecurityStateRouter.use("/dangerous-actions", dangerousActionsRouter);
+coreSecurityStateRouter.use("/email", emailDeliverabilityRouter);
 
 export const securityStateRouter = coreSecurityStateRouter;
